99011c0d11f2f0ba56a0e29700e344f0ad284735
pthole <prachi.deepak.thole@accenture.com>
2024-06-17 06:49:05 +0530

    Update pure-functions.ts
    pure function exercise

--- week2/day2/pure-functions.ts
+++ week2/day2/pure-functions.ts
@@ -8,12 +8,12 @@ const multiplyNumbers = (num1:number , num2:number) => num1 * num2;
 
 const result = multiplyNumbers(3, 2)
 // TODO log it
-console.log("Result = ", result)
+console.log("Result = ", result);
 
 // Write a pure function that concatenates firstName and lastName to say hello.
 // Then console log the result.
 
-const concatNames = (name1:string , name2:string)=> name1 + name2;
+const concatNames = (firstName: string, lastName: string): string => `${firstName} ${lastName}`;
 
-const nameResult = concatNames('Alice', 'Bloggs')
+const nameResult = concatNames('Alice', 'Bloggs');
 console.log("nameResult = ",nameResult);
